024cbfdc736932b1ea0f6b6deb5aabc023e6594e
curu28@gmail.com <curu28@gmail.com>
2024-09-04 21:15:09 -0300

    actualizando servers

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-//importar libreria
-import express from "express";
-
-//objetos para llamar los metodos de express
-
+const express = require("express");
 const app = express();
 
-//ruta inicia
+
 app.get("/", function(req,res){
         res.send("Ahora estas conectado");
 })
 
 
-//cofigurar el puerto usado para el servidor local
 app.listen(3000, function(){
-    console.log("El servidor es http://127.0.0.1:5500");
+    console.log("El servidor es http:\\localhost:3000");
 })
--- public/pages/galeria.js
+++ public/pages/galeria.js
@@ -5,7 +5,7 @@ const Galeria = () => {
       <div>
       <header>  
         <div className="holder">  
-          <img src="images/dia mundial sin tabaci.jpg" alt="Imagen del día mundial sin tabaco" />  
+          <img src="public/images/dia mundial sin tabaci.jpg" alt="Imagen del día mundial sin tabaco" />  
           <h1>No es bueno</h1>  
         </div>  
       </header>  
@@ -13,12 +13,12 @@ const Galeria = () => {
       <nav>  
         <div className="holder">  
           <ul>  
-            <li><a href="src/pages/index.html">Home</a></li>  
-            <li><a href="src/pages/temas.html">Temas</a></li>  
-            <li><a className="activo" href="src/pages/galeria.html">Galeria</a></li>  
-            <li><a href="src/pages/organizacion.html">Organizacion</a></li>  
-            <li><a href="src/pages/noticias.html">Noticias</a></li>  
-            <li><a href="src/pages/Ingresar.html">Ingresar</a></li>  
+            <li><a href="pages/index.html">Home</a></li>  
+            <li><a href="pages/temas.html">Temas</a></li>  
+            <li><a className="activo" href="pages/galeria.html">Galeria</a></li>  
+            <li><a href="pages/organizacion.html">Organizacion</a></li>  
+            <li><a href="pages/noticias.html">Noticias</a></li>  
+            <li><a href="pages/Ingresar.html">Ingresar</a></li>  
           </ul>  
         </div>  
       </nav>  
